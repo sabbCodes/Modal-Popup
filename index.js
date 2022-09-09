3a0040506298cae34c3b0025115ef963d30c4369
sabbCodes <109840351+sabbCodes@users.noreply.github.com>
2022-09-09 10:31:10 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById("open-btn").addEventListener("click", function(){
+    document.getElementById("main-popup").style.display = "block";
+})
+
+document.getElementById("close-btn").addEventListener("click", function(){
+    document.getElementById("main-popup").style.display = "none";
+})
